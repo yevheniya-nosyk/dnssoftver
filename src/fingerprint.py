cd1ec9f6a065b339a22be60046040a72acedddda
Yevheniya Nosyk <yevheniya.nosyk@gmail.com>
2023-09-08 13:23:30 +0200

    Removed 3 versions of Knot Resolver because not working

--- src/fingerprint.py
+++ src/fingerprint.py
@@ -10,6 +10,8 @@ def get_work_dir():
 def get_images(docker_client, work_dir_path):
     """Build DNS software images from Dockerfiles or pull from DockerHub"""
 
+    images = list()
+
     # Read the list of software
     with open(f"{work_dir}/software/versions_major.txt", "r") as f:
         for software in f:
@@ -25,14 +27,38 @@ def get_images(docker_client, work_dir_path):
                 # Try to build the image locally
                 try:
                     docker_client.images.build(path=f"{work_dir_path}/software/{path_to_dockerfile}", tag=image_tag, rm=True)
+                    images.append(f"{image_tag}:latest")
                 # If the path to the Dockerfile was not found (the case of some Knot Resolver versions), 
                 # we need to pull the official image provided by CZ.NIC 
                 except TypeError:
                     if vendor == "knot-resolver":
                         docker_client.images.pull(repository="cznic/knot-resolver", tag=f"v{version}")
+                        images.append(f"cznic/knot-resolver:v{version}")
                 logging.info("Processed %s-%s", vendor, version)
             else:
                 logging.info("Skipping %s-%s", vendor, version)
+
+    return images
+
+
+def run_containers(images_list, network_custom):
+    """Run the containers in our custom network"""
+
+    # Store the container objects
+    containers = list()
+    for image in images_list:
+        container_new = client.containers.run(image=image, network = network_custom, detach=True)
+        containers.append(container_new)
+
+    return containers
+
+def stop_and_remove_containers(containers_list):
+    """Stop and remove all our containers running DNS software"""
+
+    # Stop and remove each container one by one
+    for container in containers_list:
+        container.stop()
+        container.remove()
 
 
 if __name__ == '__main__':
@@ -47,10 +73,16 @@ if __name__ == '__main__':
     client = docker.from_env()
 
     # Build Docker images
-    get_images(docker_client=client, work_dir_path=work_dir)
+    images = get_images(docker_client=client, work_dir_path=work_dir)
 
     # Create a Docker network for this project
     fpdns_network = client.networks.create(name="fpdns")
+
+    # Run containers
+    containers = run_containers(images_list=images, network_custom=fpdns_network.name)
+
+    # Stop and remove containers
+    stop_and_remove_containers(containers_list=containers)
 
     # Remove the Docker network
     fpdns_network.remove()
